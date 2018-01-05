7bb448e63963ba34821f608694f3d088c23945c4
배지영 <melissaeliza0@gmail.com>
2018-01-05 17:06:39 +0900

    textview, limit text count, toolbar

--- Prject-02-Limit-TextEditor/LimitTextEditor/ViewController.swift
+++ Prject-02-Limit-TextEditor/LimitTextEditor/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textview: UITextView!
+    private var toolBar = UIToolbar()
+    private var countingText = UIBarButtonItem()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textview.delegate = self
+        addToolBar()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    func addToolBar() {
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.backgroundColor = .yellow
+        toolBar.tintColor = .black
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        countingText = UIBarButtonItem(title: "0 / 50", style: .done, target: nil, action: nil)
+        toolBar.setItems([space, countingText], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        toolBar.sizeToFit()
+        
+        textview.inputAccessoryView = toolBar
+    }
+    
+}
 
+extension ViewController : UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        countingText.title = "\(textview.text.count) / 50"
+        if 50 < textview.text.count {
+            toolBar.backgroundColor = .orange
+        } else {
+            toolBar.backgroundColor = .yellow
+        }
+    }
+    
 }
 
