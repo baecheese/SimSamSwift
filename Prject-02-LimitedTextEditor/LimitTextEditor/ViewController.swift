007f9fcd38bc8f92f66993c032345364fb409aa9
배지영 <melissaeliza0@gmail.com>
2018-01-16 14:27:28 +0900

    bug fix - 한꺼번에 많은 글자 쓰고 지울 때

--- Prject-02-LimitedTextEditor/LimitTextEditor/ViewController.swift
+++ Prject-02-LimitedTextEditor/LimitTextEditor/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
+enum LimitTextStatus {
+    case none
+    case warning
+    
+    func color() -> UIColor {
+        switch self {
+        case .none:
+            return UIColor.yellow
+        case .warning:
+            return UIColor.orange
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var textview: UITextView!
     private var toolBar = UIToolbar()
     private var countingText = UIBarButtonItem()
+    
+    private var currentStatus = LimitTextStatus.none
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +44,7 @@ class ViewController: UIViewController {
     func addToolBar() {
         toolBar.barStyle = .default
         toolBar.isTranslucent = true
-        toolBar.backgroundColor = .yellow
+        toolBar.backgroundColor = LimitTextStatus.none.color()
         toolBar.tintColor = .black
         let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         countingText = UIBarButtonItem(title: "0 / 50", style: .done, target: nil, action: nil)
@@ -45,11 +61,15 @@ extension ViewController : UITextViewDelegate {
     
     func textViewDidChange(_ textView: UITextView) {
         countingText.title = "\(textview.text.count) / 50"
-        if 51 == textview.text.count {
-            toolBar.backgroundColor = .orange
+        if 50 < textView.text.count &&
+            currentStatus == LimitTextStatus.none {
+            toolBar.backgroundColor = LimitTextStatus.warning.color()
+            currentStatus = LimitTextStatus.warning
         }
-        else if 50 == textView.text.count {
-            toolBar.backgroundColor = .yellow
+        else if textView.text.count <= 50 &&
+            currentStatus == LimitTextStatus.warning {
+            toolBar.backgroundColor = LimitTextStatus.none.color()
+            currentStatus = LimitTextStatus.none
         }
     }
     
